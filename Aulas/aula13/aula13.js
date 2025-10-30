262521a3bf5167ed1c83c326f1fb4d1de26e3cb9
liege luiza camargo <liege-6755242.rs.gov.br>
2025-10-29 21:53:32 -0300

    adicionado aos arquivos da pasta de aula14

--- Aulas/aula13/aula13.js
+++ Aulas/aula13/aula13.js
@@ -1,4 +1,4 @@
-/*let array = [11, 15, 18, 14, 12,13]
+let array = [11, 15, 18, 14, 12,13]
 let pares = 0 
 for(let i=0; i<6; i++){
     if(array[i]%2===0) {
@@ -6,11 +6,11 @@ for(let i=0; i<6; i++){
     }
 }
 console.log("a soma dos numeros pares é " + pares)
-USANDO ESSE PARA PERCORRER O INDICE DO ARRAY
-*/
+//USANDO ESSE PARA PERCORRER O INDICE DO ARRAY
+
 
 //for of 
-/*const numeros = [100, 7, 4, 8, 10, 3, 20, 777, 2000]
+const numeros = [100, 7, 4, 8, 10, 3, 20, 777, 2000]
 for (let numero of numeros){
 console.log("o numero é:", numero)
 
@@ -28,6 +28,7 @@ console.log(frase)
 
 
 //EXERCICIOS DE FIXAÇÃO 
+//for
 let valor = 0 
 for (let i = 0; i<5; i++){//começa declarando i=0, depois vai somenda de 1 e 1 até chegar em 10 , ex:
 //quando i = 0=0=0
@@ -35,7 +36,7 @@ for (let i = 0; i<5; i++){//começa declarando i=0, depois vai somenda de 1 e 1 
 //quando i = 1=1=2
 //quando i = 1=2=3
 //quando i = 3=3=6
-//quando i = 3=4=10
+//quando i = 6=4=10
 //por isso o resultado é 10
 
 
@@ -50,16 +51,16 @@ for(let numero of lista){
         console.log(numero)
     }
 }
-//foi impresso todos os numeros menos o 18 
+//foi impresso todos os numeros meaiores  o 18 
 //o FOR OF não é suficiente, o que é suficiente é usar o FOR 
 
 //EXERCICIOS DE FIXAÇÃO 3 
 const quantidadeTotal = Number (prompt("digite a quantidade de linhas:"))
 let quantidadeAtual = 0 
-while(quantidadeAtual<quantidadeTotal){//é um laço de repetição que executa um bloco de código enquanto uma condição for verdadeira
+while(quantidadeAtual<quantidadeTotal) { /*é um laço de repetição que executa um bloco de código enquanto uma condição for verdadeira*/
     let linha = "" //isso serve para criar uma linha "vazia"
-    for (let asteriscos=0;/*a repetição começa em 0*/ /*O laço continua enquanto a variável asteriscos for menor que o valor de quantidadeAtual somado a 1.// asteriscos < quantidadeAtual + 1; asteriscos++/*A cada ciclo (ou iteração), a variável asteriscos é aumentada em 1*///){
-//linha += "*"/*Dentro de cada ciclo, um asterisco ("*") é adicionado ao final da string armazenada na variável linha. */
+    for (let asteriscos=0; /*a repetição começa em 0*/ /*O laço continua enquanto a variável asteriscos for menor que o valor de quantidadeAtual somado a 1.*/ asteriscos < quantidadeAtual + 1; asteriscos++/*A cada ciclo (ou iteração), a variável asteriscos é aumentada em 1*/) {
+linha += "*" /*Dentro de cada ciclo, um asterisco ("*") é adicionado ao final da string armazenada na variável linha. */
 /* }
  console.log(linha)
  quantidadeAtual++
@@ -87,7 +88,3 @@ if /*se for*/ (quantidadeBixos === 0)/*quantidade de bixos for igual a 0*/ {
     }
     console.log("o nome de dos seus bichinhos são:", listaNomes)
 }
-
-/*let meuArray= [50, 5, 7, 8, 50]
-
-meuArray.push(50)*/ //exemplo sor lucas
---
+++ Aulas/aula14/aula14.js
@@ -0,0 +1,17 @@
+let lista =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10,]
+for(let numero of lista){
+    if (numero>10){
+
+    }
+    console.log(numero)
+}
+//fazer com que só numeros pares sejam impressos no console
+let Pares = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]//2, 4, 6, 8, 10, 12, 14, 16, 18, 20
+for (let i = 0;/*começa a contar depois do 0*/ i <= 20;/*i igual a 20*/ i++) {
+    if (i % 2 === 0) {
+      console.log(i);
+    }
+  }
+
+
+  
