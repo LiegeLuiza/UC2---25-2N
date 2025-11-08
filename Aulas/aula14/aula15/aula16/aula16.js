6b3076cb63b284cc3c61160c6e55eebd48b5659e
liege luiza camargo <liege-6755242.rs.gov.br>
2025-11-07 21:44:42 -0300

    adicionado aos arquivos da pasta de aula16

---
+++ Aulas/aula14/aula15/aula16/aula16.js
@@ -0,0 +1,15 @@
+function minhaFuncao (variavel){
+    /*return serve para restornar na função*/ return variavel * 5
+} 
+
+(minhaFuncao(2))
+(minhaFuncao(10))
+//desse jeito com o console.log roda mostrando o resultado 
+//se tirar o console.log e só deixar a função para ser invocada, ela nao roda 
+
+//atividade 23
+
+function soma (num1, num2){
+    const resultado = num1 + num2
+    
+}
