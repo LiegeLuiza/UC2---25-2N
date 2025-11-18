af80babf3245980fe037d87383b4e6a81f82e90f
liege luiza camargo <liege-6755242.rs.gov.br>
2025-11-17 21:52:15 -0300

    adicionado aos arquivos da pasta de matriz.js

---
+++ Aulas/aula19/matérianova/matriz.js
@@ -0,0 +1,30 @@
+/*let matriz = [
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+];
+
+matriz = [1][2]=1
+console.log(matriz)*/
+
+//EXEMPLO
+let matriz =[
+[1,2,3]
+[4,5,6]
+[7,8,9]
+]
+
+for(let i = 0; i <matriz.length; i++){
+    for(let j=0; j<matriz[i].length; j++){
+soma+=matriz[i][j]
+    }
+}
+console.log(matriz[i][j])
+
+//MATRIZ EXEMPLO 
+let matriz4 = [
+[1,2,3,4],
+[5,6,7,8],
+[9,10,11,12],
+[13,14,15,16]
+]
